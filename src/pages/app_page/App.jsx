7d19639c824a7f1d8e1f4ffbcef8b9e863587a61
Rubabdrani <rtbdrani@gmail.com>
2025-04-30 09:47:42 +0300

    first update

---
+++ src/pages/app_page/App.jsx
@@ -0,0 +1,17 @@
+// import Welcome from '../welcome_page/Welcome';
+import { Route } from 'react-router'
+import './App.css'
+import GenerateImage from '../generate_page/GenerateImage'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <GenerateImage/>
+    </>
+  )
+}
+
+export default App
